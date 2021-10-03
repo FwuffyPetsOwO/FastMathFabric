4fc0f3fb04e02fb7ed84ea4e095e5764a4289d0b
DestinyTheGreat <65428450+DestinyTheGreat@users.noreply.github.com>
2021-10-02 21:25:53 -0700

    Initial Commit

---
+++ src/main/java/net/FwuffyPetsOwO/FastMath/FastMathFabric.java
@@ -0,0 +1,19 @@
+package net.FwuffyPetsOwO.FastMath;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FastMathFabric implements ModInitializer {
+
+	public static final Logger LOGGER = LogManager.getLogger("FastMathFabric");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Initialized FastMathFabric");
+	}
+}
