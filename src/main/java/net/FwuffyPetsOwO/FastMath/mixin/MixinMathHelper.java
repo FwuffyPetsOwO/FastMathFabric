cab8e44bf3a5c0339beb17abb3bbc819c290207f
DestinyTheGreat <65428450+DestinyTheGreat@users.noreply.github.com>
2021-10-09 09:59:33 -0700

    Add Mixin at the (hopefully) correct location

---
+++ src/main/java/net/FwuffyPetsOwO/FastMath/mixin/MixinMathHelper.java
@@ -0,0 +1,20 @@
+package net.FwuffyPetsOwO.FastMath.mixin;
+
+import net.FwuffyPetsOwO.FastMath.FastMath;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(MathHelper.class)
+public class MixinMathHelper {
+    /**
+     * @author JellySquid
+     */
+    @Overwrite
+    public static float sin(float f) {return FastMath.sin(f);}
+    /**
+     * @author JellySquid
+     */
+    @Overwrite
+    public static float cos(float f) {return FastMath.cos(f);}
+}
